607f6fc6573b8aa741c809c1a403188521353c8c
udaykiran <udaykiran.ailapaka@novartis.com>
2016-03-09 22:57:16 +0530

    Initial Commit

---
+++ AllAbout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllAbout
+//
+//  Created by Uday Kiran Ailapaka on 09/03/16.
+//  Copyright © 2016 Uday Kiran Ailapaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AllAbout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AllAbout
+//
+//  Created by Uday Kiran Ailapaka on 09/03/16.
+//  Copyright © 2016 Uday Kiran Ailapaka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
